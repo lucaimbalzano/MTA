a589b37f3227fcf39e1d67b8a80c3d91515e18bd
luca <lucaimbalzano@gmail.com>
2021-09-29 12:50:40 +0200

    ## log4j implemented

--- src/Controller/MainController.java
+++ src/Controller/MainController.java
@@ -2,6 +2,7 @@ package Controller;
 
 import Connections.DatabaseConnections;
 
+
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -28,6 +29,8 @@ import java.sql.Statement;
 import java.util.ResourceBundle;
 
 import model.User;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 public class MainController implements Initializable {
 
@@ -61,8 +64,12 @@ public class MainController implements Initializable {
 
     String query = "SELECT * FROM user";
 
+    private static Logger logger = Logger.getLogger(MainController.class);
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
+        BasicConfigurator.configure();
+
         tcAction.setCellValueFactory(new PropertyValueFactory<>("Action"));
         tcName.setCellValueFactory(new PropertyValueFactory<>("Name"));
         tcLastname.setCellValueFactory(new PropertyValueFactory<>("Lastname"));
@@ -72,38 +79,23 @@ public class MainController implements Initializable {
         Statement statement;
         try {
 
-            data = FXCollections.observableArrayList(   );
-            int x=0;
-            User user = new User();
-
-                    statement = conn.createStatement();
+            data = FXCollections.observableArrayList();
+            statement = conn.createStatement();
             ResultSet queryOutput = statement.executeQuery(query);
+
             while(queryOutput.next()){
-              // data.add(new User(queryOutput.getString("Name"),"", queryOutput.getString("Age"), queryOutput.getString("Address"), queryOutput.getString("Phone"),"N/A"));
-//              user.setName(queryOutput.getString("Name"));
-//              data.add(user);
-                //data.add(new User(queryOutput.getString("Name"),"lastName", "12334", "Address","+39 345 123 321","N/A"));
                 data.add(new User(queryOutput.getString("Name"),queryOutput.getString("Lastname"), queryOutput.getString("Age"), queryOutput.getString("Address"),queryOutput.getString("Phone"),"N/A"));
-               System.out.println(x+" User:   "+queryOutput.getString("Name"));
-               x++;
             }
 
+            logger.debug("### Users Data Retrived ###");
+
         } catch (SQLException e) {
-            System.out.println(" ### Exception Occurred: "+e.getMessage()+" ###");
+            logger.debug(" ### Exception Occurred: "+e.getMessage()+" ###");
             e.printStackTrace();
-            System.out.println(" ### ###################################### ###");
+           logger.debug(" ### End StackTraceException ###");
         }
-
-
-//                new User("Malcom", "Ericson", "34", "34, Major Street NYC", "4350987696","Call"),
-//                new User("Mark", "Maricson", "14", "24, Major Street NYC", "0750987696","ToDo"),
-//                new User("Melman", "Ern", "34", "29, Major Street CG", "7050987696","Call"),
-//                new User("Marika", "Carricson", "24", "34, Major Street OH", "7050987096","ToDo")
-
-
         tableView.setItems(data);
-
-
+        logger.debug("### Data set into TableView ###");
     }
 
     @FXML
--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,23 @@
+import Controller.MainController;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.stage.StageStyle;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
 
 import java.util.Objects;
 
 public class Main extends Application {
+    private static Logger logger = Logger.getLogger(Main.class);
     double x, y = 0;
 
     @Override
     public void start(Stage primaryStage) throws Exception{
+        logger.debug("### Application  MTA福 Started ###");
+
         Parent root = FXMLLoader.load(getClass().getResource("/fxml/main.fxml"));
         Scene sc = new Scene(root);
 
@@ -34,6 +40,13 @@ public class Main extends Application {
 
 
     public static void main(String[] args) {
+        BasicConfigurator.configure();
         launch(args);
+    }
+
+    @Override
+    public void stop() throws Exception {
+        super.stop();
+        logger.debug("### Application  MTA福 Terminated ###");
     }
 }
