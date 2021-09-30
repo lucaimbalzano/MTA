2ce41199a11596e446cd1bdfd9d9ca63300f96ae
luca <lucaimbalzano@gmail.com>
2021-09-30 19:55:19 +0200

    ## Commit&Push delle 20.20pm , pasta alle vongole i'm coming

---
+++ src/Controller/ImportController.java
@@ -0,0 +1,48 @@
+package Controller;
+
+import Service.IUserService;
+import Service.UserServiceImpl;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.input.MouseEvent;
+import org.apache.log4j.BasicConfigurator;
+import org.apache.log4j.Logger;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.net.URL;
+import java.util.ResourceBundle;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook
+
+public class ImportController implements Initializable {
+
+    @FXML
+    Button btnImport;
+
+    private static Logger logger = Logger.getLogger(ImportController.class);
+    IUserService userServiceImpl = new UserServiceImpl();
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        BasicConfigurator.configure();
+
+    }
+
+    @FXML
+    void onClickedimportCSV(MouseEvent event) {
+
+    try{
+        FileInputStream fileIn = new FileInputStream(new File("Members.xlsx"));
+        XSSFWorkbook workbook = new XSSFWorkbook(fileIn);
+
+    }catch(FileNotFoundException fe)
+    {
+        logger.debug(" ### Exception Occurred: "+fe.getMessage()+" ###");
+        fe.printStackTrace();
+        logger.debug(" ### End StackTraceException ###");
+    }
+
+    }
+}
