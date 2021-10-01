3385dad86b6aef2cba7e4e72555db443d5f1de0e
luca <lucaimbalzano@gmail.com>
2021-10-01 14:12:35 +0200

    ## HanlderMethod Pressed,Dragged,CloseClicked - Signup/Login

--- src/Controller/LoginController.java
+++ src/Controller/LoginController.java
@@ -2,13 +2,21 @@ package Controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.PasswordField;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
@@ -26,6 +34,8 @@ public class LoginController implements Initializable {
     @FXML
     private CheckBox showPassword;
 
+    Double xCordinate, yCordinate;
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -38,7 +48,39 @@ public class LoginController implements Initializable {
 
     @FXML
     void onClickedGoToSignup(MouseEvent event) {
+        try {
+            Parent loader =  FXMLLoader.load(getClass().getResource("/fxml/signup.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(loader);
+            stage.setScene(scene);
+            stage.getIcons().add(new Image("/icons/iconLogo.png"));
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+            Stage stageLogin = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stageLogin.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+    }
 
+    @FXML
+    void onMouseDraggedLogin(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() -xCordinate);
+        stage.setY(event.getScreenY()-yCordinate);
+    }
+
+    @FXML
+    void onMousePressedLogin(MouseEvent event) {
+        this.xCordinate = event.getSceneX();
+        this.yCordinate = event.getSceneY();
+    }
+
+    @FXML
+    void onMouseClickedCloseWindowLogin(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
     }
 
 
--- src/Controller/SignupController.java
+++ src/Controller/SignupController.java
@@ -2,10 +2,17 @@ package Controller;
 
 import Encryptor.Encryptor;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import javax.crypto.BadPaddingException;
 import javax.crypto.IllegalBlockSizeException;
@@ -21,7 +28,7 @@ import java.security.NoSuchAlgorithmException;
 import java.util.HashMap;
 import java.util.ResourceBundle;
 
-public class SignupController implements Initializable
+public class SignupController implements Initializable{
 
 
     @FXML
@@ -34,6 +41,16 @@ public class SignupController implements Initializable
     private Button btnLogin, btnSigupTo;
 
 
+    File file = new File("data.csv");
+
+    //Map containing <Username, Password>
+    HashMap<String, String> loginInfo = new HashMap<>();
+
+    Encryptor encryptor = new Encryptor();
+
+    Double xCordinate, yCordinate;
+
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
 
@@ -41,20 +58,43 @@ public class SignupController implements Initializable
 
     @FXML
     void onClickedReturnToLogin(MouseEvent event) {
-
+        try {
+            Parent loader =  FXMLLoader.load(getClass().getResource("/fxml/login.fxml"));
+            Stage stage = new Stage();
+            Scene scene = new Scene(loader);
+            stage.setScene(scene);
+            stage.getIcons().add(new Image("/icons/iconLogo.png"));
+            stage.setResizable(false);
+            stage.initStyle(StageStyle.UNDECORATED);
+            stage.show();
+            Stage stageSignup = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            stageSignup.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
     }
 
     @FXML
-    void onClickedSignup(MouseEvent event) {
+    void onClickedCloseSignup(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.close();
+    }
+
+
+    @FXML
+    void onDraggedSignup(MouseEvent event) {
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setX(event.getScreenX() -xCordinate);
+        stage.setY(event.getScreenY()-yCordinate);
+    }
+
+    @FXML
+    void onPressedSignup(MouseEvent event) {
+        this.xCordinate = event.getSceneX();
+        this.yCordinate = event.getSceneY();
 
     }
 
-        File file = new File("data.csv");
-
-        //Map containing <Username, Password>
-        HashMap<String, String> loginInfo = new HashMap<>();
-
-        Encryptor encryptor = new Encryptor();
 
     private void writeToFile() throws IOException, NoSuchPaddingException, InvalidKeyException, NoSuchAlgorithmException, IllegalBlockSizeException, BadPaddingException, InvalidAlgorithmParameterException {
         String username = emailTextField.getText();
@@ -69,7 +109,8 @@ public class SignupController implements Initializable
             if(passwordTextField.isVisible()){
                 return passwordTextField.getText();
             } else {
-                return hiddenPasswordTextField.getText();
+                //return hiddenPasswordTextField.getText();
             }
+            return null;
         }
 }
